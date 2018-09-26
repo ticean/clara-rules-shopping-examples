36fda4c1a2d777a06a7ffcb1f84daf17df03be0a
Ticean Bennett <ticean@reactioncommerce.com>
2018-09-26 16:45:42 -0700

    Discount improvements

--- src/ticean/clara/shopping.clj
+++ src/ticean/clara/shopping.clj
@@ -1,4 +1,5 @@
 (ns ticean.clara.shopping
+  "Demonstration of Clara rules applied to a commerce application."
   (:require
     [clara.rules.accumulators :as acc]
     [clara.rules :refer [defquery defrule fire-rules insert insert!
@@ -7,27 +8,34 @@
 
 ;;;; Facts used in the examples below.
 
-(defrecord Order [year month day])
 (defrecord Customer [status])
-(defrecord Discount [code name description type value])
-(defrecord Promotion [reason type])
+(defrecord Order [year month day])
+(defrecord OrderPromoCode [code])
 (defrecord OrderLineItem [sku cost])
 (defrecord OrderTotal [total])
+
+(defrecord Discount [code name description type value])
+(defrecord Promotion [reason type])
 
 
 ;;;; Some example rules. ;;;;
 
-(defrule total-purchases
+(defrule order-total
   [?total <- (acc/sum :cost) :from [OrderLineItem]]
   =>
   (insert! (->OrderTotal ?total)))
 
+
 ;;;Discounts.
-(defrule summer-special
+
+(defrule summer-discount
   "Place an order in the summer and get 20% off!"
   [Order (#{:june :july :august} month)]
   =>
-  (insert! (->Discount :summer-special "Summer special." "" :percent 20)))
+  (insert! (->Discount :summer-discount
+                       "Summer discount."
+                       "Place an order in the summer and get 20% off."
+                       :percent 20)))
 
 (defrule vip-discount
   "VIPs get a discount on purchases over $100. Cannot be combined with any
@@ -35,7 +43,10 @@
   [Customer (= status :vip)]
   [OrderTotal (> total 100)]
   =>
-  (insert! (->Discount :vip "VIP Discount" "" :percent 10)))
+  (insert! (->Discount :vip
+                       "VIP Discount"
+                       "VIPs get a 10% discount on purchases over $100."
+                       :percent 10)))
 
 (def max-discount
   "Accumulator that returns the highest percentage discount."
@@ -45,9 +56,16 @@
 (defquery get-best-discount
   "Query to find the best discount that can be applied"
   []
-  [?discount <- max-discount :from [Discount]])
+  [?best-discount <- max-discount :from [Discount]])
+
+(defquery get-all-discounts
+  "Query to find the best discount that can be applied"
+  []
+  [?discount <- Discount])
+
 
 ;;; Promotions.
+
 (defrule free-widget-month
   "All purchases over $200 in August get a free widget."
   [Order (= :august month)]
@@ -73,8 +91,10 @@
   "Print the discounts from the given session."
   [session]
   (println "Printing Discounts:")
-  (doseq [{discount :?discount} (query session get-best-discount)]
-    (println "   " discount))
+  (doseq [{best-discount :?best-discount} (query session get-best-discount)]
+    (println "   Best Discount: " best-discount))
+  (doseq [{discount :?discount} (query session get-all-discounts)]
+    (println "   All Discounts: " discount))
   session)
 
 (defn print-promotions!
@@ -88,20 +108,25 @@
 (defn run-examples
   "Function to run the above example."
   []
+
   (println "\nStarting Clara session 1.")
+  (println "   Expects :summer-discount and and :vip-discount.")
+  (println "   Expects no promotions.\n")
   (-> (mk-session 'ticean.clara.shopping :cache false)
       (insert (->Customer :vip)
-              (->Order 2013 :march 20)
+              (->Order 2018 :july 20)
               (->OrderLineItem :gizmo 20)
-              (->OrderLineItem :widget 20))
+              (->OrderLineItem :widget 120))
 
       (fire-rules)
       (print-discounts!))
 
   (println "\nStarting Clara session 2.")
-  (-> (mk-session 'ticean.clara.shopping)
+  (println "   Expects :summer-discount.\n")
+  (println "   Expects :free-widget-month and :free-lunch-with-gizmo.\n")
+  (-> (mk-session 'ticean.clara.shopping :cache false)
       (insert (->Customer :not-vip)
-              (->Order 2013 :august 20)
+              (->Order 2018 :august 20)
               (->OrderLineItem :gizmo 20)
               (->OrderLineItem :widget 120)
               (->OrderLineItem :widget 90))
