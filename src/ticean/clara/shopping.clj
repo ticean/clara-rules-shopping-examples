f4c550a5da2ce481b673bd44e5638ba1769e2c7c
Ticean Bennett <ticean@reactioncommerce.com>
2018-10-27 14:25:12 -0700

    Remove fact type from map to record conversion

--- src/ticean/clara/shopping.clj
+++ src/ticean/clara/shopping.clj
@@ -36,19 +36,21 @@
 
 
 (defn fact-record-type [m] (-> m :fact-type name symbol))
+(defn- map->fact-type [m conversion-fn]
+  (-> m conversion-fn (dissoc :fact-type)))
 
 (defmulti ->fact-record
   "Converts a map representing a fact to a fact Record. Note that not all facts
   are insertable because they should be inferred by Clara."
   fact-record-type)
 
-(defmethod ->fact-record 'Customer [m] (map->Customer m))
-(defmethod ->fact-record 'Discount [m] (map->Discount m))
-(defmethod ->fact-record 'Order [m] (map->Order m))
-(defmethod ->fact-record 'OrderLineItem [m] (map->OrderLineItem m))
-(defmethod ->fact-record 'OrderPromoCode [m] (map->OrderPromoCode m))
-(defmethod ->fact-record 'ShippingMethod [m] (map->ShippingMethod m))
-(defmethod ->fact-record 'SelectedShippingMethod [m] (map->SelectedShippingMethod m))
+(defmethod ->fact-record 'Customer [m] (map->fact-type m map->Customer))
+(defmethod ->fact-record 'Discount [m] (map->fact-type m map->Discount))
+(defmethod ->fact-record 'Order [m] (map->fact-type m map->Order))
+(defmethod ->fact-record 'OrderLineItem [m] (map->fact-type m map->OrderLineItem))
+(defmethod ->fact-record 'OrderPromoCode [m] (map->fact-type m map->OrderPromoCode))
+(defmethod ->fact-record 'ShippingMethod [m] (map->fact-type m map->ShippingMethod))
+(defmethod ->fact-record 'SelectedShippingMethod [m] (map->fact-type m map->SelectedShippingMethod))
 (defmethod ->fact-record :default [m]
   (throw (ex-info "Unknown fact type"
                   {:input-type (fact-record-type m)
